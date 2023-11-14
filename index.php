9a469605dfcd686bd441c404449c5b2cf77b8039
ingridih <ingrid_centioli@yahoo.com.br>
2023-11-14 17:56:57 -0300

    ok

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <title>Criar Orçamento.</title>
     </head>
     <body>
-        <div class="container">
+        <div class="container" style="margin-top: 50px;">
             
                 <div class="">
                     <div class="row">
@@ -24,11 +24,7 @@
                     <div class="row">
                         <div class="mb-3 col">
                             <label for="exampleInputEmail1" class="form-label">Empresa</label>
-                            <input type="text" class="form-control" id="empresa" placeholder="Entre com a empresa que vai receber o orçamento.">
-                        </div>
-                        <div class="mb-3 col">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <input type="text" class="form-control" id="empresa" name="empresa" placeholder="Entre com a empresa que vai receber o orçamento.">
                         </div>
                     </div>
                 </div>
@@ -38,15 +34,23 @@
                         <div data-repeater-list="kt_docs_repeater_basic">
                             <div data-repeater-item>
                                 <div class="form-group row">
-                                    <div class="col-md-3">
-                                        <label class="form-label">Name:</label>
-                                        <input type="email" class="form-control mb-2 mb-md-0" placeholder="Enter full name" />
+                                    <div class="col-md-6">
+                                        <label class="form-label">Materiais/ Peças e equipamentos</label>
+                                        <input type="text" class="form-control mb-2 mb-md-0" id="item" name="item"   placeholder="Entre com o nome do item e detalhes" />
+                                    </div>  
+                                    <div class="col-md-2">
+                                        <label class="form-label">Quantidade</label>
+                                        <input type="number" class="form-control mb-2 mb-md-0 quantidade" id="quantidade" name="quantidade" placeholder="Entre com a quantidade" />
                                     </div>
-                                    <hr>
-
-                                    
-                                    <div class="col-md-4">
-                                        <a href="javascript:;" data-repeater-delete class="btn btn-sm btn-light-danger mt-3 mt-md-8">
+                                    <div class="col-md-2">
+                                        <label class="form-label">Valor</label>
+                                        <input type="text" class="form-control mb-2 mb-md-0 valor" id="valor" name="valor" placeholder="Entre com o valor" />
+                                    </div>
+                                    <div class="col-md-1">
+                                        <span class="totalt"></span>
+                                    </div>
+                                    <div class="col-md-1" style="margin-top: 20px;">
+                                        <a href="javascript:;" data-repeater-delete class="btn btn-sm btn btn-danger mt-3 mt-md-8">
                                             <i class="ki-duotone ki-trash fs-5"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>
                                             Delete
                                         </a>
@@ -56,13 +60,14 @@
                         </div>
                     </div>
                     <div class="form-group mt-5">
-                        <a href="javascript:;" data-repeater-create class="btn btn-light-primary">
+                        <a href="javascript:;" data-repeater-create class="btn btn-primary">
                             <i class="ki-duotone ki-plus fs-3"></i>
-                            Add
+                            Adicionar Linha
                         </a>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Gerar Orçamento</button>
+                <hr>
+                <button type="submit" class="btn btn-success">Gerar Orçamento</button>
             </form>
         </div>
     </body>
@@ -71,25 +76,60 @@
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery.repeater.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.repeater.min.js"></script>
+<script src="js/jquery.maskMoney.min.js"></script>
+
 
 <script>
-    $(document).ready(function() {
-        $('#kt_docs_repeater_basic').repeater({
-            initEmpty: false,
+$(document).ready(function() {
 
-            defaultValues: {
-                'text-input': 'foo'
-            },
+    $(".valor").maskMoney({thousands:'.', decimal:','});
 
-            show: function () {
-                $(this).slideDown();
-            },
+    $('#kt_docs_repeater_basic').repeater({
+        initEmpty: false,
 
-            hide: function (deleteElement) {
-                $(this).slideUp(deleteElement);
-            }
-        });
+        defaultValues: {
+            'text-input': 'foo'
+        },
+
+        show: function () {
+            $(this).slideDown();
+            $(".valor").maskMoney({thousands:'.', decimal:','});
+        },
+
+        hide: function (deleteElement) {
+            $(this).slideUp(deleteElement);
+        }
+    });
 });
+
+$('.valor').on('keyup', function() {
+    CalculaTotais();
+});
+
+
+function CalculaTotais() {
+    var ValorFinal = 0;
+    var totalt = document.getElementsByClassName("totalt");
+
+    for (var i = 0; i < totalt.length; i++) {
+        var qtd = parseFloat(document.getElementsByClassName('quantidade')[i].value);
+        var valor = parseFloat(document.getElementsByClassName('valor')[i].value.replace(",", "."));
+
+        if (!isNaN(qtd) && !isNaN(valor)) {
+            var valortotal = qtd * valor;
+
+            const formatoBr_valorprod = valortotal.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+            });
+
+            totalt[i].innerHTML = formatoBr_valorprod;
+
+           
+        }
+    }
+
+}
 </script>
